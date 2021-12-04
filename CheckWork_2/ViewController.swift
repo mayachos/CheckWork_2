181c683b56b619f1dd18043e8365bf7c74400513
mayachos <amacrk@icloud.com>
2021-12-04 18:47:37 +0900

    Initial Commit

---
+++ CheckWork_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckWork_2
+//
+//  Created by maya on 2021/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
